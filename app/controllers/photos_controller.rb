714073125a3caa392d77612ec87e452253a96cf7
Pratham Gandhi <pratham@uchicago.edu>
2023-02-16 22:54:55 +0000

    doing more

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,5 +6,9 @@ class PhotosController < ApplicationController
   end
 
   def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos.at(0)
+    render({ :template => "photo_templates/show.html.erb" })
   end
 end
